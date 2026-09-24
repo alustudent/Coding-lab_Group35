76cf711f45c5a2c08c29c300b37f3d95166ce4c6
jansonhush <j.ndayizeye1@alustudent.com>
2026-09-24 02:48:00 -0700

    Implement initialize_system to create missing dir

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -4,7 +4,28 @@
 
 # Janson: create active_logs, archived_logs and reports if missing
 initialize_system() {
-    :
+
+# Creating directory active-logs if it's not there!
+	if [ -d "active_logs" ]; then
+		echo "active_logs already exists!"
+	else
+		echo "Creating active_logs directory"
+		mkdir active_logs
+	fi
+# Creating archived_logs directory if it's not there
+	if [ -d "archived_logs" ]; then
+		echo "archived_logs alreaady exists"
+	else
+		echo "Creating archived_logs"
+		mkdir archived_logs
+	fi
+# Creating reports dir if it's not there!
+	if [ -d "reports" ]; then
+		echo "reports already exists"
+	else
+		echo "Creating reports"
+		mkdir reports
+	fi
 }
 
 # Gahima: restrict active_logs to owner read/write and display permissions
